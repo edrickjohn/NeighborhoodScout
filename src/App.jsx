f8836fe759c4e797acca87884d36bcd021f6918f
kaltstein <109489007+kaltstein@users.noreply.github.com>
2023-11-18 17:25:04 +0800

    changed to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div>
-      <BrowserRouter basename="/NeighborhoodScout">
+      <HashRouter>
         <Routes>
           <Route index path="/" element={<Login />}></Route>
           <Route index path="/home" element={<Home />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
