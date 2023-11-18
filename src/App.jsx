79d29d1d3ec4456f82d02b70407788e7eee6639e
kaltstein <109489007+kaltstein@users.noreply.github.com>
2023-11-18 10:12:17 +0800

    change browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
 function App() {
-  return <div className=" min-h-screen">Neigborhood</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Login />}></Route>
+          <Route index path="/home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Pages/Home/Home.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
