44eed68b329527bb1782a3040c37d2f05e1bce1e
kaltstein <109489007+kaltstein@users.noreply.github.com>
2023-11-18 17:27:02 +0800

    added button click on image

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const AuthLayout = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <div className="max-w-7xl mx-auto text-white   bg-white min-h-[100vh] ">
       <nav className=" bg-secondary">
-        <img src="real_estate_logo.png" alt="logo" className="h-20 bg-white" />
+        <img
+          src="real_estate_logo.png"
+          alt="logo"
+          className="h-20 bg-white"
+          onClick={(e) => {
+            navigate("/");
+          }}
+        />
       </nav>
 
       <div className="">{children}</div>
